william-gr/middleware

commit 85e7c668d9ca129ab55d985eb951f101c2a9a738

NFS bind IP should be a picklist, no allow arbitrary IPs
Ticket:	#3492

Former-commit-id: e9b6b6f4bfc3b7e52ed59f0f58b0ed7236c932bf

gui/freeadmin/static/lib/js/freeadmin.js
@@ -92,6 +92,7 @@ require([
     "dijit/ProgressBar",
     "dijit/Tooltip",
     "dojox/form/BusyButton",
+    "dojox/form/CheckedMultiSelect",
     "dojox/grid/EnhancedGrid",
     "dojox/grid/enhanced/plugins/DnD",
     "dojox/grid/enhanced/plugins/Menu",

gui/services/forms.py
@@ -217,8 +217,16 @@ class AFPForm(ModelForm):
 
 class NFSForm(ModelForm):
 
+    nfs_srv_bindip = forms.MultipleChoiceField(
+        label=models.NFS._meta.get_field('nfs_srv_bindip').verbose_name,
+        help_text=models.NFS._meta.get_field('nfs_srv_bindip').help_text,
+        required=False,
+        widget=forms.widgets.CheckedMultiSelect(),
+    )
+
     class Meta:
         model = models.NFS
+        exclude = ('nfs_srv_bindip', )
         widgets = {
             'nfs_srv_mountd_port': forms.widgets.TextInput(),
             'nfs_srv_rpcstatd_port': forms.widgets.TextInput(),
@@ -227,6 +235,14 @@ class NFSForm(ModelForm):
 
     def __init__(self, *args, **kwargs):
         super(NFSForm, self).__init__(*args, **kwargs)
+        self.fields['nfs_srv_bindip'].choices = list(choices.IPChoices())
+        self.fields['nfs_srv_bindip'].initial = (
+            self.instance.nfs_srv_bindip.encode('utf-8').split(',') if self.instance.id
+            else ''
+        )
+        self.fields.keyOrder.remove('nfs_srv_bindip')
+        self.fields.keyOrder.insert(2, 'nfs_srv_bindip')
+
         self.fields['nfs_srv_mountd_port'].label = (
             self.fields['nfs_srv_mountd_port'].label.lower()
         )
@@ -242,8 +258,7 @@ class NFSForm(ModelForm):
         if not ips:
             return ''
         bind = []
-        for ip in ips.split(','):
-            ip = ip.strip()
+        for ip in ips:
             try:
                 IPAddress(ip.encode('utf-8'))
             except:
@@ -254,7 +269,9 @@ class NFSForm(ModelForm):
         return ','.join(bind)
 
     def save(self):
-        super(NFSForm, self).save()
+        obj = super(NFSForm, self).save(commit=False)
+        obj.nfs_srv_bindip = self.cleaned_data.get('nfs_srv_bindip')
+        obj.save()
         started = notifier().restart("nfs")
         if (
             started is False

commit 397dc88a94e65f77b7ee3984e182f9f1b9f00098

Fix javascript regression introduced in 85e7c668d9 [formerly e9b6b6f4bf]
Ticket:	#3169

Former-commit-id: 3f4438a183b847197d4a68a9e2b82e34aba13568

gui/freeadmin/static/lib/js/freeadmin.js
@@ -169,6 +169,7 @@ require([
     ProgressBar,
     Tooltip,
     BusyButton,
+    CheckedMultiSelect,
     EnhancedGrid,
     enhancedDnD,
     enhancedMenu,