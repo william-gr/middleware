william-gr/middleware

commit 1f138c444562ca91682143e1dd8b90396b13aebf

Teardown and recreate vlan interfaces when networking is restarted.
Ticket: #3708

By doing this any and all changes to a vlan interface are immediately
done when the GUI is changed.  The previous behavior was open to
a number of bugs that could be caused by renaming the interface or
changing it's parent interface.  In the old behavior only a reboot
would ensure the correct configuration.


Former-commit-id: 8b261d4e9761db5b8c6c09ba9dfe9ee67aa02711

gui/middleware/notifier.py
@@ -426,6 +426,11 @@ class notifier:
                 self._system("/sbin/sysctl net.inet6.ip6.auto_linklocal=1")
                 self._system("/usr/sbin/service autolink auto_linklocal quietstart")
                 self._system("/usr/sbin/service netif stop")
+        interfaces = self._pipeopen("ifconfig -l")
+        interface_list = interfaces.split(" ")
+        for interface in interface_list:
+            if interface.startswith("vlan"):
+                self._system("ifconfig %s destroy" % interface)
         self._system("/etc/netstart")
         self._system("/usr/sbin/service rtsold start")
 

commit cba5daff39504d02d0ebfe09cbefc9e26bd94615

Fix "typo" introduced in 1f138c444562 [formerly 8b261d4e9761]
Ticket:	#3754

Former-commit-id: ab098f437db8a5852757c80d3445f21679d43506

gui/middleware/notifier.py
@@ -426,7 +426,7 @@ class notifier:
                 self._system("/sbin/sysctl net.inet6.ip6.auto_linklocal=1")
                 self._system("/usr/sbin/service autolink auto_linklocal quietstart")
                 self._system("/usr/sbin/service netif stop")
-        interfaces = self._pipeopen("ifconfig -l")
+        interfaces = self._pipeopen("ifconfig -l").communicate()[0]
         interface_list = interfaces.split(" ")
         for interface in interface_list:
             if interface.startswith("vlan"):