william-gr/middleware

commit f40b459674f3d894be45123a1abd8ece61e587ff

Add done method and hook for normal forms
Former-commit-id: 9e2a5306f142d884da4294916c13705a124e848c

gui/common/forms.py
@@ -156,3 +156,7 @@ class Form(AdvMixin, F):
             row_ender=u'</td></tr>',
             help_text_html=u'<br />%s',
             errors_on_separate_row=False)
+
+    def done(self, request, events):
+        fname = str(type(self).__name__)
+        appPool.hook_form_done(fname, self, request, events)

commit df8b063472fa5d0b97e6594b581f838dd2303fe8

Fix API breakage introduced in f40b459674f [formerly 9e2a5306f14]
Now heading to the beach for carnival! :)

Ticket:	#4420

Former-commit-id: a777ef593036fe6dc63b9b1910054e1dce2e3bbd

gui/common/forms.py
@@ -157,6 +157,6 @@ class Form(AdvMixin, F):
             help_text_html=u'<br />%s',
             errors_on_separate_row=False)
 
-    def done(self, request, events):
+    def done(self, request=None, events=None, **kwargs):
         fname = str(type(self).__name__)
         appPool.hook_form_done(fname, self, request, events)