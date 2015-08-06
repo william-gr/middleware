ed6f70778e079269f11f4ffe7d55e1c9454fe343
Jakub Klama <jakub@ixsystems.com>
2015-08-06 15:05:13 +0200

    Add support for checking multiple values at once with an "in" operator.

--- src/datastore/drivers/mongodb/mongodb.py
+++ src/datastore/drivers/mongodb/mongodb.py
@@ -81,7 +81,10 @@ class MongodbDatastore(object):
 
         if op in self.operators_table:
             if op in ('in', 'nin'):
-                return {name: {self.operators_table[op]: [value]}}
+                if isinstance(value, (list, tuple)):
+                    return {name: {self.operators_table[op]: value}}
+                else:
+                    return {name: {self.operators_table[op]: [value]}}
 
             return {name: {self.operators_table[op]: value}}
 
