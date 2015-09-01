1fbbb437cdd3622884b172cdd7d9b857d7fea01c
William Grzybowski <wg@FreeBSD.org>
2015-09-01 09:29:33 -0300

    Change guest to guest_enable
    Ticket:	#11257

--- src/dispatcher/plugins/AFPPlugin.py
+++ src/dispatcher/plugins/AFPPlugin.py
@@ -73,7 +73,7 @@ def _init(dispatcher, plugin):
     plugin.register_schema_definition('service-afp', {
         'type': 'object',
         'properties': {
-            'guest': {'type': 'boolean'},
+            'guest_enable': {'type': 'boolean'},
             'guest_user': {'type': 'string'},
             'bind_addresses': {
                 'type': ['array', 'null'],
