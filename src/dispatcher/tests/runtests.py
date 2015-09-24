8d9a562adcd26c7a21f5aaed186e6a45e54056cd
Lilit <lilit@ixsystems.com>
2015-09-23 18:00:49 -0700

    Report failures

--- src/dispatcher/tests/runtests.py
+++ src/dispatcher/tests/runtests.py
@@ -4,9 +4,21 @@ import sys
 loader = unittest.TestLoader()
 r = loader.discover('./')
 results = unittest.TestResult()
+results.buffer = True
 r.run(results)
 
-print 'Result is ' + str(results)
-print "Tests ran " + str(results.testsRun)
-print "Tests result: " + str(results.wasSuccessful())
+overall = results.wasSuccessful()
 
+print '========================='
+print "Total tests discovered:  " + str(r.countTestCases())
+print "Total tests ran: " + str(results.testsRun)
+print "Overall Result: " + str(overall)
+print '========================='
+if not overall:
+    print '========================================='
+    print "The Failed Tests reported these errors:"
+    print '========================================='
+    for error in results.failures:
+	    print str(error[0]) + ': \n' + error[1]
+
+#print results.errors
