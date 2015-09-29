e575436d7522bf9b7a8cdb206b9d61e5c5e12f7d
William Grzybowski <wg@FreeBSD.org>
2015-09-29 15:20:41 -0300

    Fix tftp service to generate/restart
    Ticket:	#11719

--- src/dispatcher/plugins/TFTPPlugin.py
+++ src/dispatcher/plugins/TFTPPlugin.py
@@ -62,8 +62,8 @@ class TFTPConfigureTask(Task):
         try:
             node = ConfigNode('service.tftp', self.configstore)
             node.update(tftp)
-            self.dispatcher.call_sync('etcd.generation.generate_group', 'tftp')
-            self.dispatcher.call_sync('services.restart', 'tftp')
+            self.dispatcher.call_sync('etcd.generation.generate_group', 'services')
+            self.dispatcher.call_sync('services.restart', 'tftpd')
             self.dispatcher.dispatch_event('service.tftp.changed', {
                 'operation': 'updated',
                 'ids': None,
