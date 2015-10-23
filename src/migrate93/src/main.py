67c80d7ab107d1af49a47a4462e7133e7f18ba60
William Grzybowski <wg@FreeBSD.org>
2015-10-22 22:06:32 -0200

    Add previous dir to the path

--- src/migrate93/src/main.py
+++ src/migrate93/src/main.py
@@ -1,4 +1,7 @@
 import os
+import sys
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'freenasUI.settings')
 # Make sure to load all modules
