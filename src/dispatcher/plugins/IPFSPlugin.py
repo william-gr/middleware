977874377a3551d5880e8ae1c0a8622667e3230a
Suraj Ravichandran <suraj@ixsystems.com>
2015-10-28 00:53:37 -0700

    Only move the contents of the ipfs path dir and not the entire top level path.
    The top level path maybe a dataset amongs other thing hence this decision was taken by me and jkh.

--- src/dispatcher/plugins/IPFSPlugin.py
+++ src/dispatcher/plugins/IPFSPlugin.py
@@ -26,8 +26,8 @@
 import errno
 import logging
 import os
+import shutil
 
-from lib.system import SubprocessException, system
 from datastore.config import ConfigNode
 from dispatcher.rpc import RpcException, SchemaHelper as h, description, accepts, returns
 from task import Task, Provider, TaskException, ValidationException
@@ -71,23 +71,26 @@ class IPFSConfigureTask(Task):
     def run(self, ipfs):
         try:
             node = ConfigNode('service.ipfs', self.configstore)
-            old_path = node.path
-            node.update(ipfs)
+            old_path = node['path'].value
             if 'path' in ipfs and ipfs['path'] != old_path:
-                if old_path and os.path.exists(old_path):
-                    try:
-                        system('mv', old_path, ipfs['path'])
-                    except SubprocessException as err:
-                        raise TaskException(
-                            errno.EIO,
-                            "Error while migrating ipfs path: {0}".format(err.err)
-                        )
-                else:
-                    if not os.path.exists(ipfs['path']):
-                        os.makedirs(ipfs['path'])
+                if not os.path.exists(ipfs['path']):
+                    os.makedirs(ipfs['path'])
                     # jkh says that the ipfs path should be owned by root
                     os.chown(ipfs['path'], 0, 0)
-
+                # Only move the contents and not the entire folder
+                # there could be other stuff in that folder
+                # (a careless user might have merged this with his other files)
+                # also this folder could be a dataset in which case a simple move will fail
+                # so lets just move the internal contents of this folder over
+                if os.path.exists(old_path):
+                    try:
+                        for item in os.listdir(old_path):
+                            shutil.move(old_path + '/' + item, ipfs['path'])
+                    except shutil.Error as serr:
+                        raise TaskException(
+                            errno.EIO,
+                            "Migrating ipfs path resulted in error: {0}".format(serr))
+            node.update(ipfs)
             self.dispatcher.call_sync('etcd.generation.generate_group', 'services')
             self.dispatcher.call_sync('services.apply_state', 'ipfs', True)
             self.dispatcher.dispatch_event('service.ipfs.changed', {
