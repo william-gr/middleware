1f3f9c82ab1cb1772b123c500ffba389718a1516
William Grzybowski <wg@FreeBSD.org>
2015-11-16 09:43:44 -0200

    Add some useful options to dispatcher client setuptools

--- src/dispatcher/client/python/setup.py
+++ src/dispatcher/client/python/setup.py
@@ -1,8 +1,19 @@
 from setuptools import setup
 
+
 setup(
     name='dispatcher-client',
     version='10.0',
     description='Dispatcher client library',
-    packages=['dispatcher']
+    packages=['dispatcher'],
+    license='BSD',
+    platforms='any',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+    ],
 )
